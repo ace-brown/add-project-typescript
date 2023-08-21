89055d41f50a625bf5a974a08c91c939f7ed2e93
ace-brown <kazem_abdi@yahoo.com>
2023-08-21 22:26:41 +0200

    add: state management class

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,35 @@
+// Project State management - store project data and push them to ProjectList class to display them
+class ProjectState {
+  private static instance: ProjectState;
+  private projects: any[] = [];
+  private listeners: any[] = [];
+
+  private constructor() {}
+
+  static getInstane() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  addListener(listnerFunc: Function) {
+    this.listeners.push(listnerFunc);
+  }
+
+  addProject(title: string, desc: string, NumOfPeople: number) {
+    const newProject = {
+      id: Math.random().toString(),
+      title: title,
+      description: desc,
+      people: NumOfPeople,
+    };
+    this.projects.push(newProject);
+  }
+}
+
+const projectState = ProjectState.getInstane();
 // Input Validation
 interface Validatable {
   value: string | number;
@@ -9,20 +41,77 @@ interface Validatable {
 }
 
 function validate(validateInput: Validatable) {
-  let isVlaid;
+  let isVlaid = true;
 
   if (validateInput.required) {
     isVlaid = isVlaid && validateInput.value.toString().trim().length !== 0;
   }
 
-  if (validateInput.minLength && typeof validateInput.value === "string") {
+  if (
+    validateInput.minLength != null &&
+    typeof validateInput.value === "string"
+  ) {
     isVlaid = isVlaid && validateInput.value.length > validateInput.minLength;
+  }
+
+  if (
+    validateInput.maxLength != null &&
+    typeof validateInput.value === "string"
+  ) {
+    isVlaid = isVlaid && validateInput.value.length < validateInput.maxLength;
+  }
+
+  if (validateInput.min != null && typeof validateInput.value === "number") {
+    isVlaid = isVlaid && validateInput.value > validateInput.min;
+  }
+
+  if (validateInput.max != null && typeof validateInput.value === "number") {
+    isVlaid = isVlaid && validateInput.value < validateInput.max;
+  }
+
+  return isVlaid;
+}
+
+// ProjectList class - responsible for outputing projects on the webpage
+class ProjectList {
+  projectListTemplateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  projectElement: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.projectListTemplateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedFormNode = document.importNode(
+      this.projectListTemplateElement.content,
+      true
+    );
+    this.projectElement = importedFormNode.firstElementChild as HTMLElement;
+    this.projectElement.id = `${this.type}-projects`;
+
+    // Function call
+    this.attach();
+    this.renderContent();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.projectElement);
+  }
+
+  private renderContent() {
+    // Setting #(aka id) for ul element
+    const listId = `${this.type}-project-list`;
+    this.projectElement.querySelector("ul")!.id = listId;
+    this.projectElement.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECT";
   }
 }
 
+// ProjectInput class - show the input field on the page and passing user data to state management class
 class ProjectInput {
   // Types
-  templateElement: HTMLTemplateElement;
+  inputTemplateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   formElement: HTMLFormElement;
   titleInputElement: HTMLInputElement;
@@ -31,12 +120,12 @@ class ProjectInput {
 
   constructor() {
     // Inputs
-    this.templateElement = document.getElementById(
+    this.inputTemplateElement = document.getElementById(
       "project-input"
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
     const importedFormNode = document.importNode(
-      this.templateElement.content,
+      this.inputTemplateElement.content,
       true
     );
     this.formElement = importedFormNode.firstElementChild as HTMLFormElement;
@@ -63,11 +152,26 @@ class ProjectInput {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
-    if (
-      validate({ value: enteredTitle, required: true, minLength: 5 }) &&
-      validate({ value: enteredDescription, required: true, minLength: 5 }) &&
-      validate({ value: enteredPeople, required: true, minLength: 5 })
-    ) {
+    const titleObj: Validatable = {
+      value: enteredTitle,
+      required: true,
+      minLength: 5,
+    };
+
+    const descObj: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+
+    const peopleObj: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (!validate(titleObj) || !validate(descObj) || !validate(peopleObj)) {
       alert("Invalid input, please try again!");
       return;
     } else {
@@ -87,8 +191,10 @@ class ProjectInput {
     const userInfo = this.gatherUserInformation();
     if (Array.isArray(userInfo)) {
       const [title, desc, people] = userInfo;
+      // passing user data to state management class
+      projectState.addProject(title, desc, people);
+      this.clearInput();
     }
-    this.clearInput();
   }
 
   private clearInput() {
@@ -103,3 +209,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activeProject = new ProjectList("active");
+const finishedProject = new ProjectList("finished");
